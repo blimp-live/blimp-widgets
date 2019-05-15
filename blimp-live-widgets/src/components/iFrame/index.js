385901adc3a912bb9e19e59f79c945df3ce227a4
heoegema <hollyoegema@gmail.com>
2019-05-15 13:38:01 -0400

    Adding Starter files for iFrame widget

---
+++ blimp-live-widgets/src/components/iFrame/index.js
@@ -0,0 +1,11 @@
+// Future iFrame Widget Implement goes here
+import React, { Component } from 'react'
+
+export default class iFrame extends Component { 
+	render() { 
+		return { 
+			
+ 		} 
+
+	} 
+} 
