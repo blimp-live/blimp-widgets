c348eef66d3e375a30f2b0bc74911485d2e1ecf5
heoegema <hollyoegema@gmail.com>
2019-05-15 13:54:48 -0400

    Adding starter files for Clock widget

--- blimp-live-widgets/src/index.js
+++ blimp-live-widgets/src/index.js
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types'
 // Import your widget here
 import HelloWorld from './components/HelloWorld';
 import ExampleComponent from './components/ExampleComponent';
+import Clock from './components/Clock';
 
 // Export it here
 export {
   HelloWorld,
-  ExampleComponent
+  ExampleComponent,
+  Clock
 }
