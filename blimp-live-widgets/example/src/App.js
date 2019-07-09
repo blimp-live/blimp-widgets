ffb7f61f9f46a21edb96b2b95b22ff2b777fba67
Bhavika Sharma <sharma.bhavika@hotmail.com>
2019-07-09 15:58:08 -0400

    Add styles to text

--- blimp-live-widgets/example/src/App.js
+++ blimp-live-widgets/example/src/App.js
@@ -6,7 +6,15 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent
+          text='Modern React component module'
+          size="40px"
+          font="Arial"
+          color="Purple"
+          background_color="Pink"
+          border_color="Green"
+          align="center"
+          />
         <HelloWorld />
         <IFrameComponent url="https://www.youtube.com/embed/h_m-BjrxmgI" />
         <Clock width={300} theme={ClockThemes.navy} showSmallTicks={false}/>
