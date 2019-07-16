fb22567e042f4e1b97f65640cd5470bd9a640e29
BhavikaSharma <BhavikaSharma@users.noreply.github.com>
2019-07-16 13:28:27 -0400

    Add styles to text (#5)

--- blimp-live-widgets/src/components/ExampleComponent/index.js
+++ blimp-live-widgets/src/components/ExampleComponent/index.js
@@ -5,16 +5,35 @@ import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
   static propTypes = {
-    text: PropTypes.string
+    text: PropTypes.string,
+    size: PropTypes.string,
+    font: PropTypes.string,
+    color: PropTypes.string,
+    background_color: PropTypes.string,
+    border_color: PropTypes.string,
+    align: PropTypes.string,
   }
 
   render() {
     const {
-      text
+      text,
+      size,
+      font,
+      color,
+      background_color,
+      border_color,
+      align,
     } = this.props
 
     return (
-      <div className={styles.test}>
+      <div className={styles.test}
+           style={{
+             "font-size" : size,
+             "font-family": font,
+             "color": color,
+             "background-color": background_color,
+             "border-color": border_color,
+             "text-align": align}}>
         Example Component: {text}
       </div>
     )
