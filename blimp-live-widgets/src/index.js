dddf7707bdc40c7492dc976ff633fe7ff8870db8
John Huang <JohnjunHH@gmail.com>
2019-09-10 23:46:09 -0400

    Cleaned up merge conflict

--- blimp-live-widgets/src/index.js
+++ blimp-live-widgets/src/index.js
@@ -10,7 +10,6 @@ import Countdown from './components/Countdown';
 import ScrollingText from './components/ScrollingText';
 import TwitterComponent from './components/Twitter';
 import Weather from './components/Weather';
-<<<<<<< 51fa9c10bd23d839a150b9902b593c75fb163e4c
 import Gallery from './components/Gallery';
 import DateComponent from './components/DateComponent';
 
