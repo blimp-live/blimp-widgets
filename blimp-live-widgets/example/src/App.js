040286d952a651efd17847e512b3b0775c258ad1
jj8huang <JohnjunHH@gmail.com>
2019-09-10 20:58:39 -0700

    Created and slightly styled date widget (#18)
    * Created and slightly styled date widget
    
    * Created and slightly styled date widget
    
    * Cleaned up merge conflict
    
    * MERGED AGAIN

--- blimp-live-widgets/example/src/App.js
+++ blimp-live-widgets/example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {ExampleComponent, HelloWorld, IFrameComponent, Clock, ClockThemes, Countdown, ScrollingText, TwitterComponent, Weather, Gallery} from 'blimp-live-widgets'
+import {ExampleComponent, HelloWorld, IFrameComponent, Clock, ClockThemes, Countdown, ScrollingText, TwitterComponent, Weather, Gallery, DateComponent} from 'blimp-live-widgets'
 
 export default class App extends Component {
 
@@ -32,6 +32,7 @@ export default class App extends Component {
         <ScrollingText text='Here is a test' />
         <TwitterComponent account="HackTheNorth" />
         <Weather city="Toronto" units="metric"/>
+        <DateComponent />
       </div>
     )
   }
