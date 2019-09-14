8b9a71acf79bdb941819629520e903b4777821b5
Samuel San <samuelsaninbox@gmail.com>
2019-09-13 19:54:45 -0700

    attempt 2

--- blimp-live-widgets/src/components/iFrame/index.js
+++ blimp-live-widgets/src/components/iFrame/index.js
@@ -3,7 +3,7 @@ import styles from './styles.css'
 
 export default class IFrameComponent extends Component {
 	render() {
-		var sourceUrl = this.props.url ? this.props.url : "https://www.youtube.com/watch?v=NJbKtYZCk4U";
+		var sourceUrl = this.props.url ? this.props.url : "https://www.youtube.com/embed/NJbKtYZCk4U";
 		console.log(sourceUrl);
 		return (
 			<IFrame source={sourceUrl} />
