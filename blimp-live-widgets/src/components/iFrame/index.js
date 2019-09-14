e9198edead88dbba1485c66e644b525b5418d4b0
Bhavika Sharma <bsharma@splunk.com>
2019-09-13 22:17:07 -0700

    Update npm version to 1.1.3

--- blimp-live-widgets/src/components/iFrame/index.js
+++ blimp-live-widgets/src/components/iFrame/index.js
@@ -3,7 +3,7 @@ import styles from './styles.css'
 
 export default class IFrameComponent extends Component {
 	render() {
-		var sourceUrl = this.props.url ? this.props.url : "https://www.youtube.com/embed/NJbKtYZCk4U?autoplay=1";
+		var sourceUrl = this.props.url ? this.props.url : "https://www.youtube.com/embed/s_FhBuN769I?autoplay=1;&loop=1;&playlist=s_FhBuN769I";
 		console.log(sourceUrl);
 		return (
 			<IFrame source={sourceUrl} />
@@ -12,13 +12,13 @@ export default class IFrameComponent extends Component {
 }
 
 const IFrame = ({ source }) => {
-  if (!source) {
-  	return <div>iFrame not found</div>;
-  }
-  const src = source;
-  return (
-  	<div className="embedded-iframe" style ={{width:'100%'}}>
-    	<iframe src={src}></iframe>
-    </div>
-  );
+	if (!source) {
+		return <div>iFrame not found</div>;
+	}
+	const src = source;
+	return (
+		<div className="embedded-iframe" style={{ width: '100%' }}>
+			<iframe src={src}></iframe>
+		</div>
+	);
 };
